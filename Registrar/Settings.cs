0c7c6e145488044e27e8140fb37b2a2e319e6551
awilliams17 <awilliams17411@gmail.com>
2018-09-07 15:49:16 -0500

    constructor for settings?

--- Registrar/Settings.cs
+++ Registrar/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Permissions;
 using Microsoft.Win32;
 
 namespace Registrar
@@ -11,8 +12,16 @@ namespace Registrar
         // Subkeys have Values
 
         // So I need the Keyname, Subkeyname (optional), to create the registry instance.
-
+        RegistryKey _rootKey = null;
+        RegistryKey _registry = null;
 
         public Object[] SettingsArray = new Object[] { };
+
+        public Settings(RegistryKey root_key, string sub_key)
+        {
+            _rootKey = root_key;
+            _registry = _rootKey.OpenSubKey(sub_key);
+        }
+
     }
 }
