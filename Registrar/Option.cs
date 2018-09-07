8261ade3dcd8e68a8d56555febdd122a8ac90948
awilliams17 <awilliams17411@gmail.com>
2018-09-07 16:14:30 -0500

    I think this is going to work maybe

--- Registrar/Option.cs
+++ Registrar/Option.cs
@@ -2,11 +2,38 @@
 
 namespace Registrar
 {
-    public class Option<T>
+    public class Option
     {
-        public Option(string key_name, Type type, Validator[] validators, T value)
-        {
+        private string _keyName = null;
+        private string _subKey = null;
+        private Type _optionType = null;
+        private Validator[] _validatorList = null;
+        private object _optionValue = null;
 
+        public Option(string key_name, string sub_key, Type type, Validator[] validators, Object value)
+        {
+            _keyName = key_name;
+            _subKey = sub_key;
+            _optionType = type;
+            _validatorList = validators;
+            _optionValue = value;
         }
+
+        public bool RunValidators()
+        {
+            foreach (Validator validator in _validatorList)
+            {
+                if (!validator.Validate(_optionValue))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public abstract class Validator
+    {
+        public abstract bool Validate(object value);
     }
 }
--- Registrar/Settings.cs
+++ Registrar/Settings.cs
@@ -15,7 +15,7 @@ namespace Registrar
         RegistryKey _rootKey = null;
         RegistryKey _registry = null;
 
-        public Object[] SettingsArray = new Object[] { };
+        public Option[] SettingsArray = new Option[] { };
 
         public Settings(RegistryKey root_key, string sub_key)
         {
@@ -23,5 +23,31 @@ namespace Registrar
             _registry = _rootKey.OpenSubKey(sub_key);
         }
 
+        public void LoadSettings() // Load settings from the registry instance
+        {
+            if (_registry == null)
+            {
+                // Throw new exception
+            }
+
+        }
+
+        public void SaveSettings() // Save the settings array values to the registry
+        {
+            if (_registry == null)
+            {
+                // Throw new exception
+            }
+
+            foreach (Option option in SettingsArray)
+            {
+                bool option_valid = option.RunValidators();
+                if (!option_valid)
+                {
+                    // Throw new exception
+                }
+                // Save it to the Registry
+            }
+        }
     }
 }
