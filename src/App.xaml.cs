bd01d8a5aee8dce2b252aab939dd70692624357b
Emilian Roman <roman.emilian@outlook.com>
2019-04-12 16:11:43 +0800

    Initialise repository

---
+++ src/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SPV3.GUI
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App
+  {
+  }
+}
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SPV3.GUI
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
