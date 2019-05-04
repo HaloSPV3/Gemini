3ca4d9cc4dfb5040363bb2f22f037e6f5511d327
Emilian Roman <roman.emilian@outlook.com>
2019-05-05 02:19:22 +0800

    Use buttons for presets instead of slider

--- src/SettingsWindow.xaml.cs
+++ src/SettingsWindow.xaml.cs
@@ -23,5 +23,50 @@ namespace SPV3
     {
       Close();
     }
+
+    private void SetPreset0(object sender, RoutedEventArgs e)
+    {
+      _settings.Level = 0;
+    }
+
+    private void SetPreset1(object sender, RoutedEventArgs e)
+    {
+      _settings.Level = 1;
+    }
+
+    private void SetPreset2(object sender, RoutedEventArgs e)
+    {
+      _settings.Level = 2;
+    }
+
+    private void SetPreset3(object sender, RoutedEventArgs e)
+    {
+      _settings.Level = 3;
+    }
+
+    private void SetPreset4(object sender, RoutedEventArgs e)
+    {
+      _settings.Level = 4;
+    }
+
+    private void SetPreset5(object sender, RoutedEventArgs e)
+    {
+      _settings.Level = 5;
+    }
+
+    private void SetPreset6(object sender, RoutedEventArgs e)
+    {
+      _settings.Level = 6;
+    }
+
+    private void SetPreset7(object sender, RoutedEventArgs e)
+    {
+      _settings.Level = 7;
+    }
+
+    private void SetPreset8(object sender, RoutedEventArgs e)
+    {
+      _settings.Level = 8;
+    }
   }
 }
