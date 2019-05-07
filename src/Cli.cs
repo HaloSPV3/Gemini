3eaa17dd677887afcb9f02a546fec273b598f8e6
Emilian Roman <roman.emilian@outlook.com>
2019-05-08 07:21:26 +0800

    Enforce screenshot mode when loading HXE through the SPV3 launcher

--- src/Cli.cs
+++ src/Cli.cs
@@ -33,7 +33,7 @@ namespace SPV3
     
     public static Exit.Code Start(string args)
     {
-      args = args + $" -path {Paths.Directories.Data} -console -devmode";
+      args = args + $" -path {Paths.Directories.Data} -console -devmode -screenshot";
       var process = Process.Start(Executable, args);
 
       if (process == null)
