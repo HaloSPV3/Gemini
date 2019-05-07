d26a754ff8225a61bbbf3e076537a3c15f7c3f77
Emilian Roman <roman.emilian@outlook.com>
2019-05-08 05:51:36 +0800

    Update preset buttons' state depending on the chosen preset

--- src/SettingsWindow.xaml.cs
+++ src/SettingsWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using System.Windows;
+using HXE.SPV3;
 
 namespace SPV3
 {
@@ -12,6 +13,8 @@ namespace SPV3
       InitializeComponent();
       _settings = (Settings) DataContext;
       _settings.Load();
+
+      RefreshPresetButtons();
     }
 
     private void Save(object sender, RoutedEventArgs e)
@@ -27,47 +30,118 @@ namespace SPV3
 
     private void SetPreset0(object sender, RoutedEventArgs e)
     {
-      _settings.Main.Level = 0;
+      SetPresetLevel(0);
     }
 
     private void SetPreset1(object sender, RoutedEventArgs e)
     {
-      _settings.Main.Level = 1;
+      SetPresetLevel(1);
     }
 
     private void SetPreset2(object sender, RoutedEventArgs e)
     {
-      _settings.Main.Level = 2;
+      SetPresetLevel(2);
     }
 
     private void SetPreset3(object sender, RoutedEventArgs e)
     {
-      _settings.Main.Level = 3;
+      SetPresetLevel(3);
     }
 
     private void SetPreset4(object sender, RoutedEventArgs e)
     {
-      _settings.Main.Level = 4;
+      SetPresetLevel(4);
     }
 
     private void SetPreset5(object sender, RoutedEventArgs e)
     {
-      _settings.Main.Level = 5;
+      SetPresetLevel(5);
     }
 
     private void SetPreset6(object sender, RoutedEventArgs e)
     {
-      _settings.Main.Level = 6;
+      SetPresetLevel(6);
     }
 
     private void SetPreset7(object sender, RoutedEventArgs e)
     {
-      _settings.Main.Level = 7;
+      SetPresetLevel(7);
     }
 
     private void SetPreset8(object sender, RoutedEventArgs e)
     {
-      _settings.Main.Level = 8;
+      SetPresetLevel(8);
+    }
+
+    private void SetPresetLevel(int level)
+    {
+      _settings.Main.Level = level;
+      RefreshPresetButtons();
+    }
+
+    private void RefreshPresetButtons()
+    {
+      Preset0Button.IsEnabled = true;
+      Preset1Button.IsEnabled = true;
+      Preset2Button.IsEnabled = true;
+      Preset3Button.IsEnabled = true;
+      Preset4Button.IsEnabled = true;
+      Preset5Button.IsEnabled = true;
+      Preset6Button.IsEnabled = true;
+      Preset7Button.IsEnabled = true;
+      Preset8Button.IsEnabled = true;
+
+      var mxao = _settings.Main.Mxao;
+      var dof  = _settings.Main.Dof;
+      var mb   = _settings.Main.MotionBlur;
+      var lf   = _settings.Main.DynamicLensFlares;
+      var vol  = _settings.Main.Volumetrics;
+      var ld   = _settings.Main.LensDirt;
+
+      if (mxao  == PostProcessing.MxaoOptions.Off && dof == PostProcessing.DofOptions.Off &&
+          mb    == PostProcessing.MotionBlurOptions.Off
+          && lf == false && vol == false && ld)
+        Preset0Button.IsEnabled = false;
+
+      if (mxao  == PostProcessing.MxaoOptions.Off && dof == PostProcessing.DofOptions.Off &&
+          mb    == PostProcessing.MotionBlurOptions.Off
+          && lf == false && vol && ld)
+        Preset1Button.IsEnabled = false;
+
+      if (mxao  == PostProcessing.MxaoOptions.Off && dof == PostProcessing.DofOptions.Low &&
+          mb    == PostProcessing.MotionBlurOptions.BuiltIn
+          && lf == false && vol && ld)
+        Preset2Button.IsEnabled = false;
+
+      if (mxao  == PostProcessing.MxaoOptions.Low && dof == PostProcessing.DofOptions.Low &&
+          mb    == PostProcessing.MotionBlurOptions.BuiltIn
+          && lf == false && vol && ld)
+        Preset3Button.IsEnabled = false;
+
+      if (mxao == PostProcessing.MxaoOptions.Low && dof == PostProcessing.DofOptions.Low &&
+          mb   == PostProcessing.MotionBlurOptions.PombLow
+          && lf && vol && ld)
+        Preset4Button.IsEnabled = false;
+
+      if (mxao == PostProcessing.MxaoOptions.Low && dof == PostProcessing.DofOptions.High &&
+          mb   == PostProcessing.MotionBlurOptions.PombLow
+          && lf && vol && ld)
+        Preset5Button.IsEnabled = false;
+
+      if (mxao == PostProcessing.MxaoOptions.High && dof == PostProcessing.DofOptions.High &&
+          mb   == PostProcessing.MotionBlurOptions.PombLow
+          && lf && vol && ld)
+        Preset6Button.IsEnabled = false;
+
+      if (mxao == PostProcessing.MxaoOptions.High && dof == PostProcessing.DofOptions.High &&
+          mb   == PostProcessing.MotionBlurOptions.PombHigh
+          && lf && vol && ld)
+        Preset7Button.IsEnabled = false;
+
+      if (mxao  == PostProcessing.MxaoOptions.Off && dof == PostProcessing.DofOptions.Off &&
+          mb    == PostProcessing.MotionBlurOptions.Off
+          && lf == false && vol == false && ld == false)
+        Preset8Button.IsEnabled = false;
     }
 
     private void GetAmaiSosu(object sender, RoutedEventArgs e)
