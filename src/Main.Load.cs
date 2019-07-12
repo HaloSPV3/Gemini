e4fbaa122ceff099380f6fd296c26ee0fe53e8e2
Emilian Roman <roman.emilian@outlook.com>
2019-07-12 10:17:44 +0800

    Remove Start & Back buttons from the SPV3 Preset Input
    -   they achieve absolutely nothing
    -   they make hxe bark about duplicate keys

--- src/Main.Load.cs
+++ src/Main.Load.cs
@@ -154,9 +154,7 @@ namespace SPV3
             {Profile.ProfileInput.Action.ThrowGrenade, Profile.ProfileInput.Button.LB},
             {Profile.ProfileInput.Action.Flashlight, Profile.ProfileInput.Button.LT},
             {Profile.ProfileInput.Action.MeleeAttack, Profile.ProfileInput.Button.RB},
-            {Profile.ProfileInput.Action.FireWeapon, Profile.ProfileInput.Button.RT},
-            {Profile.ProfileInput.Action.MenuAccept, Profile.ProfileInput.Button.Start},
-            {Profile.ProfileInput.Action.MenuBack, Profile.ProfileInput.Button.Back}
+            {Profile.ProfileInput.Action.FireWeapon, Profile.ProfileInput.Button.RT}
           };
 
           profile.Save();
