11a7eab2fc508ddaa36123030ad86a65f47cbbec
Emilian Roman <roman.emilian@outlook.com>
2020-11-04 18:55:11 +0800

    Add static Kernel class with Infer method
    This commit introduces the static Kernel class, which currently exposes
    an Infer method that returns an enum representing the mode that HXE will
    run in.
    
    USAGE
    -----
    
        // conditional
        if (Kernel.Infer() == Mode.SPV33)
          // spv3.3-related goodies
    
        // switch
        switch (Kernel.Infer())
        {
          case Mode.HCE:
            // this honestly should never happen
            // but it's here for just in case scenarios
            break;
          case Mode.SPV32:
            // hxe will run in spv3.2/legacy mode
            break;
          case Mode.SPV33:
            // hxe will run in spv3.3 mode
            break;
          default:
            // once again should never happen
        }

---
+++ src/Kernel.cs
@@ -0,0 +1,30 @@
+﻿using static System.IO.File;
+
+namespace SPV3
+{
+  /**
+   * Object representing the HXE Kernel.
+   */
+  public static class Kernel
+  {
+    /**
+     * Available HXE kernel modes.
+     */
+    public enum Mode
+    {
+      HCE,   /* vanilla hce mode -- won't ever be used by SPV3        */
+      SPV32, /* <=SPV3.2 "Legacy" mode, without post-processing       */
+      SPV33  /* >=SPV3.3 mode, with post-processing and other goodies */
+    }
+
+    /**
+     * Infer the mode that the HXE kernel will use in the current environment.
+     */
+    public static Mode Infer()
+    {
+      return Exists(HXE.Paths.Version)
+        ? Mode.SPV33
+        : Mode.SPV32;
+    }
+  }
+}
