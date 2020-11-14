1189f0436c8fc88615bbad135a3592a2945cd2ef
Emilian Roman <roman.emilian@outlook.com>
2020-11-14 11:59:40 +0800

    Update copyright headers

--- src/Configuration.UserControl.xaml.cs
+++ src/Configuration.UserControl.xaml.cs
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2019 Emilian Roman
+ * Copyright (c) 2020 Noah Sherwin
  * 
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
