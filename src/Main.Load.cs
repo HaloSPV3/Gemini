ff413f28434dc147bfafbbce69935315eadc3551
Noah Sherwin <noahrsherwin@gmail.com>
2021-02-12 05:08:58 -0800

    SPV3: Only append -window parameter when Display Mode is NOT fullscreen
    The old method worked fine, but this skips the middleman (Window bool)

--- src/Main.Load.cs
+++ src/Main.Load.cs
@@ -29,6 +29,7 @@ using HXE.HCE;
 using HXE.SPV3;
 using SPV3.Annotations;
 using File = System.IO.File;
+using Loader = SPV3.Configuration.ConfigurationLoader;
 
 namespace SPV3
 {
@@ -129,7 +130,7 @@ namespace SPV3
             Width       = Kernel.spv3.ResolutionEnabled ? Kernel.spv3.Width  : (ushort) 0,
             Height      = Kernel.spv3.ResolutionEnabled ? Kernel.spv3.Height : (ushort) 0,
             Refresh     = Kernel.spv3.Vsync ? Kernel.spv3.Framerate : (ushort) 0,
-            Window      = Kernel.spv3.Window,
+            Window      = Kernel.spv3.DisplayMode != (byte) Loader.DisplayModes.Fullscreen,
             Adapter     = (byte) (Kernel.spv3.Adapter + 1),
             NoGamma     = Kernel.hxe.Video.GammaOn == false
           },
