661a7d19fe575cca4859225d0e39b25705748fad
Noah Sherwin <noahrsherwin@gmail.com>
2021-02-13 03:25:57 -0800

    Remove unused function DRMPatch()

--- src/Install.cs
+++ src/Install.cs
@@ -262,12 +262,6 @@ namespace SPV3
     
     public event PropertyChangedEventHandler PropertyChanged;
 
-    // CLEANUP
-    private void DRMPatch()
-    {
-      new Patcher().Write(Kernel.hxe.Tweaks.Patches, Path.Combine(Target, HXE.Paths.HCE.Executable));
-    }
-
     public void Initialise()
     {
       Main = Visible;
