8473ce4a0e5d1483dfe97074ece81ae16678711f
Noah Sherwin <noahrsherwin@gmail.com>
2021-02-13 17:47:25 -0800

    Update Install UserControl conditions

--- src/Install.UserControl.xaml.cs
+++ src/Install.UserControl.xaml.cs
@@ -32,12 +32,9 @@ namespace SPV3
 
     public Install_UserControl()
     {
-      if (System.IO.File.Exists(System.IO.Path.Combine(Paths.Packages(Environment.CurrentDirectory), HXE.Paths.Manifest)))
-      {
-        InitializeComponent();
-        _install = (Install) DataContext;
-        _install.Initialise();
-      }
+      InitializeComponent();
+      _install = (Install) DataContext;
+      _install.Initialise();
     }
 
     public event EventHandler Home;
