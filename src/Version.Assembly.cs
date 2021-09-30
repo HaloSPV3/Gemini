251e93010fa579dfbd8c7680275795a009f9d365
Noah Sherwin <noahrsherwin@gmail.com>
2021-09-30 11:45:33 -0700

    style: set indentation to tabs style: format document whitespace

--- src/Version.Assembly.cs
+++ src/Version.Assembly.cs
@@ -28,85 +28,85 @@ using static System.Reflection.Assembly;
 
 namespace SPV3
 {
-  public partial class Version
-  {
-    public class VersionAssembly : INotifyPropertyChanged
-    {
-      private string _address;
-      private string _content;
-      private System.Version _version;
+	public partial class Version
+	{
+		public class VersionAssembly : INotifyPropertyChanged
+		{
+			private string _address;
+			private string _content;
+			private System.Version _version;
 
-      private Visibility _visibility = Visibility.Collapsed;
+			private Visibility _visibility = Visibility.Collapsed;
 
-      public Visibility Visibility
-      {
-        get => _visibility;
-        set
-        {
-          if (value == _visibility) return;
-          _visibility = value;
-          OnPropertyChanged();
-        }
-      }
+			public Visibility Visibility
+			{
+				get => _visibility;
+				set
+				{
+					if (value == _visibility) return;
+					_visibility = value;
+					OnPropertyChanged();
+				}
+			}
 
-      public string Content
-      {
-        get => _content;
-        set
-        {
-          if (value == _content) return;
-          _content = value;
-          OnPropertyChanged();
-        }
-      }
+			public string Content
+			{
+				get => _content;
+				set
+				{
+					if (value == _content) return;
+					_content = value;
+					OnPropertyChanged();
+				}
+			}
 
-      public string Address
-      {
-        get => _address;
-        set
-        {
-          if (value == _address) return;
-          _address = value;
-          OnPropertyChanged();
-        }
-      }
+			public string Address
+			{
+				get => _address;
+				set
+				{
+					if (value == _address) return;
+					_address = value;
+					OnPropertyChanged();
+				}
+			}
 
-      public System.Version Version
-      {
-        get => _version;
-        set
-        {
-          if (value == _version) return;
-          _version = value;
-          OnPropertyChanged();
-        }
-      }
+			public System.Version Version
+			{
+				get => _version;
+				set
+				{
+					if (value == _version) return;
+					_version = value;
+					OnPropertyChanged();
+				}
+			}
 
-      public event PropertyChangedEventHandler PropertyChanged;
+			public event PropertyChangedEventHandler PropertyChanged;
 
-      public void Initialise()
-      {
-        var version = GetExecutingAssembly()?.GetName().Version;
-        if (version.Major == 0) return;
+			public void Initialise()
+			{
+				var version = GetExecutingAssembly()?.GetName().Version;
+				if (version.Major == 0) return;
 
-        var refHash = new Func<string>(() =>
-        {
-            using (var stream = GetExecutingAssembly().GetManifestResourceStream("SPV3.hash"))
-            using (var reader = new StreamReader(stream ?? throw new InvalidOperationException()))
-                return reader.ReadToEnd();
-        })();
+				var refHash = new Func<string>(() =>
+				{
+					using (var stream = GetExecutingAssembly().GetManifestResourceStream("SPV3.hash"))
+					using (var reader = new StreamReader(stream ?? throw new InvalidOperationException()))
+						return reader.ReadToEnd();
+				})();
 
-        Version    = version;
-        Content    = $"Version {version}-{refHash.ToUpper()}";
-        Address    = $"https://github.com/HaloSPV3/SPV3-Loader/commit/{refHash}";
-        Visibility = Visibility.Visible;
-      }
+				Version = version;
+				Content = $"Version {version}-{refHash.ToUpper()}";
+				Address = $"https://github.com/HaloSPV3/SPV3-Loader/commit/{refHash}";
+				Visibility = Visibility.Visible;
+			}
 
-      [NotifyPropertyChangedInvocator]
-      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-      {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-      }
-    }
-  }
+			[NotifyPropertyChangedInvocator]
+			protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+			{
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
 }
