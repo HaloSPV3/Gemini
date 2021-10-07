f654ef93c42aba4be5725b66055225517bf203a6
Noah Sherwin <noahrsherwin@gmail.com>
2021-10-07 16:11:42 -0700

    style: format document 'Version.Upstream.cs'

--- src/Version.Upstream.cs
+++ src/Version.Upstream.cs
@@ -28,97 +28,97 @@ using static HXE.Net.DefaultHttpClient;
 
 namespace SPV3
 {
-  public partial class Version
-  {
-    public class VersionUpstream : INotifyPropertyChanged
+    public partial class Version
     {
-      private const string Latest = "https://github.com/HaloSPV3/HCE/raw/meta/latest.xml";
-      private string _address;
-      private string _content;
-      private System.Version _version;
-
-      private Visibility _visibility = Visibility.Collapsed;
-
-      public Visibility Visibility
-      {
-        get => _visibility;
-        set
+        public class VersionUpstream : INotifyPropertyChanged
         {
-          if (value == _visibility) return;
-          _visibility = value;
-          OnPropertyChanged();
+            private const string Latest = "https://github.com/HaloSPV3/HCE/raw/meta/latest.xml";
+            private string _address;
+            private string _content;
+            private System.Version _version;
+
+            private Visibility _visibility = Visibility.Collapsed;
+
+            public Visibility Visibility
+            {
+                get => _visibility;
+                set
+                {
+                    if (value == _visibility) return;
+                    _visibility = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string Content
+            {
+                get => _content;
+                set
+                {
+                    if (value == _content) return;
+                    _content = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string Address
+            {
+                get => _address;
+                set
+                {
+                    if (value == _address) return;
+                    _address = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            // TODO: rename to NewVersion
+            public System.Version Version
+            {
+                get => _version;
+                set
+                {
+                    if (value == _version) return;
+                    _version = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public async void Initialise()
+            {
+                try
+                {
+                    var oldTimeout = Client.Timeout;
+                    Client.Timeout = TimeSpan.FromSeconds(10);
+                    using (var rm = await Client.GetAsync(Latest))
+                    using (var rs = await rm.Content.ReadAsStreamAsync())
+                    using (var sr = new StreamReader(rs
+                                                     ?? throw new NullReferenceException("There was a 'success' response from the server, but the expected content was not found.")))
+                    {
+                        var serverVersion = System.Version.Parse(sr.ReadLine()?.TrimEnd()
+                                                     ?? throw new NullReferenceException("Could not infer server-side version."));
+                        var clientVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+
+                        Version = serverVersion;
+                        Content = $"Update loader to {serverVersion}";
+                        Visibility = serverVersion > clientVersion ? Visibility.Visible : Visibility.Collapsed;
+                        Address = sr.ReadLine()?.TrimEnd() ?? throw new NullReferenceException("Update manifest did not contain file URL.");
+                    }
+                    Client.Timeout = oldTimeout;
+                }
+                catch (Exception)
+                {
+                    Visibility = Visibility.Collapsed;
+                }
+            }
+
+            [NotifyPropertyChangedInvocator]
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
-      }
-
-      public string Content
-      {
-        get => _content;
-        set
-        {
-          if (value == _content) return;
-          _content = value;
-          OnPropertyChanged();
-        }
-      }
-
-      public string Address
-      {
-        get => _address;
-        set
-        {
-          if (value == _address) return;
-          _address = value;
-          OnPropertyChanged();
-        }
-      }
-
-      // TODO: rename to NewVersion
-      public System.Version Version
-      {
-        get => _version;
-        set
-        {
-          if (value == _version) return;
-          _version = value;
-          OnPropertyChanged();
-        }
-      }
-
-      public event PropertyChangedEventHandler PropertyChanged;
-
-      public async void Initialise()
-      {
-        try
-        {
-          var oldTimeout = Client.Timeout;
-          Client.Timeout = TimeSpan.FromSeconds(10);
-          using (var rm = await Client.GetAsync(Latest))
-          using (var rs = await rm.Content.ReadAsStreamAsync())
-          using (var sr = new StreamReader(rs
-                                           ?? throw new NullReferenceException("There was a 'success' response from the server, but the expected content was not found.")))
-          {
-            var serverVersion = System.Version.Parse(sr.ReadLine()?.TrimEnd()
-                                          ?? throw new NullReferenceException("Could not infer server-side version."));
-            var clientVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-
-            Version = serverVersion;
-            Content    = $"Update loader to {serverVersion}";
-            Visibility = serverVersion > clientVersion ? Visibility.Visible : Visibility.Collapsed;
-            Address    = sr.ReadLine()?.TrimEnd() ?? throw new NullReferenceException("Update manifest did not contain file URL.");
-          }
-          Client.Timeout = oldTimeout;
-        }
-        catch (Exception)
-        {
-          Visibility = Visibility.Collapsed;
-        }
-      }
-
-      [NotifyPropertyChangedInvocator]
-      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-      {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-      }
     }
-  }
 }
