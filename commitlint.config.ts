592adedb946361bd925702d91a6559c1a1e8ed99
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-17 17:57:33 -0700

    chore(commitlint): add general scopes; copy descriptions from AmaiSosu, HXE

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,20 +1,29 @@
-import hceShared from "@halospv3/hce.shared-config/commitlintConfig";
+import hceShared from '@halospv3/hce.shared-config/commitlintConfig';
 
 const scopes = {
   CHANGELOG: 'Affects "CHANGELOG.md".',
-  deps: 'Affects any runtime dependencies.',
-  'deps-dev': 'Affects dependencies not included at runtime.',
-  README: 'Affects "README.md".',
+  commitlint: 'Affects this repo\'s commitlint config, esp. its commit scopes.',
+  contributing: 'Affects CONTRIBUTING.md',
+  'conv-pr': 'Affects ".github/workflows/conv-pull-requests.yml".',
+  deps: 'Affects dependencies bundled with or depended on by published packages and artifacts. '
+    + 'For NuGet package/PackageReferences, this means anything that has "runtime", "native" or '
+    + '"contentfiles" included.',
+  'deps-dev': 'Affects dependencies used by CI, dev environments, or the build system(s); '
+    + 'but are not required at runtime nor bundled with or statically linked into the published '
+    + 'binaries or packages. For NuGet packages/PackageReferences, this would be anything with'
+    + 'PrivateAssets="All" and no "runtime", "native", or "contentfiles" to be included in output.',
+  README: 'Affects README.md or any other README documents.',
   release: 'Reserved for release commits.',
   Version: 'Affects "src/Version.*".',
-  vscode: 'Affects "./vscode/**/*".'
+  TODO: 'Affects TODO.md or any todo comments.',
+  vscode: 'Affects "./vscode/**/*".',
 };
 
-hceShared.rules["scope-enum"] = [
+hceShared.rules['scope-enum'] = [
   2,
   'always',
   Reflect.ownKeys(scopes)
-    .map(key => String(key)) as (keyof typeof scopes)[]
+    .map(key => String(key)),
 ];
 
 export default hceShared;
