fb3d31f2e3913a20e32ad2a3e9fa722bef20361a
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-22 18:18:40 -0700

    chore(commitlint): add scope `App`

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,6 +1,7 @@
 import hceShared from '@halospv3/hce.shared-config/commitlintConfig';
 
 const scopes = {
+  App: 'Affects "src/App.*"',
   CHANGELOG: 'Affects "CHANGELOG.md".',
   commitlint: 'Affects this repo\'s commitlint config, esp. its commit scopes.',
   Configuration: 'Affects "src/Configuration*".',
