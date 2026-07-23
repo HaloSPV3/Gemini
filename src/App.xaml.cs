1032d159abd05f224dc00290f2b6a8f77eb4eb30
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-22 18:17:25 -0700

    style(App): format App.xaml.cs

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -24,20 +24,20 @@ using System.Windows;
 
 namespace SPV3
 {
-    /// <summary>
-    ///   Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
+  /// <summary>
+  ///   Interaction logic for App.xaml
+  /// </summary>
+  public partial class App
+  {
+    protected override void OnStartup(StartupEventArgs e)
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            if (e.Args.Any(arg => arg.Equals("-auto")))
-            {
-                new Main().Invoke();
-                Environment.Exit(0);
-            }
+      if (e.Args.Any(arg => arg.Equals("-auto")))
+      {
+        new Main().Invoke();
+        Environment.Exit(0);
+      }
 
-            base.OnStartup(e);
-        }
+      base.OnStartup(e);
     }
+  }
 }
