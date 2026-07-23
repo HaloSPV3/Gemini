dac4bd07d6f129e72894db879b9d30721da47178
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-23 03:58:41 -0700

    chore(commitlint): simplify scope descriptions

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,16 +1,16 @@
 import hceShared from '@halospv3/hce.shared-config/commitlintConfig';
 
 const scopes = {
-  AmaiSosu: 'Affects "src/AmaiSosu.cs".',
-  App: 'Affects "src/App.*".',
-  CHANGELOG: 'Affects "CHANGELOG.md".',
-  commitlint: 'Affects this repo\'s commitlint config, esp. its commit scopes.',
-  Compile: 'Affects "src/Compile.*".',
-  Configuration: 'Affects "src/Configuration*".',
-  Context: 'Affects "src/Context.cs".',
+  AmaiSosu: 'src/AmaiSosu.cs',
+  App: 'src/App.*',
+  CHANGELOG: 'CHANGELOG.md',
+  commitlint: 'This repo\'s commitlint config, esp. its commit scopes.',
+  Compile: 'src/Compile.*',
+  Configuration: 'src/Configuration*',
+  Context: 'src/Context.cs',
   contributing: 'Affects CONTRIBUTING.md.',
-  ControllerPreset: 'Affects "src/ControllerPreset*".',
-  'conv-pr': 'Affects ".github/workflows/conv-pull-requests.yml".',
+  ControllerPreset: 'src/ControllerPreset*',
+  'conv-pr': '.github/workflows/conv-pull-requests.yml',
   deps: 'Affects dependencies bundled with or depended on by published packages and artifacts. '
     + 'For NuGet package/PackageReferences, this means anything that has "runtime", "native" or '
     + '"contentfiles" included.',
@@ -18,20 +18,20 @@ const scopes = {
     + 'but are not required at runtime nor bundled with or statically linked into the published '
     + 'binaries or packages. For NuGet packages/PackageReferences, this would be anything with'
     + 'PrivateAssets="All" and no "runtime", "native", or "contentfiles" to be included in output.',
-  Information: 'Affects "src/Information.*".',
-  Install: 'Affects "src/Main.Install.cs", its section in "src/Main.Window.*", or "src/Install*".',
-  Kernel: 'Affects "src/Kernel*".',
-  Main: 'Affects "src/Main.*".',
-  News: 'Affects "src/News.*".',
-  Paths: 'Affects "src/Paths.cs".',
-  README: 'Affects README.md or any other README documents.',
-  Report: 'Affects "src/Report.*".',
+  Information: 'src/Information.*',
+  Install: 'src/Main.Install.cs, src/Main.Window.*, or src/Install*',
+  Kernel: 'src/Kernel*',
+  Main: 'src/Main.*',
+  News: 'src/News.*',
+  Paths: 'src/Paths.cs',
+  README: 'README.md or any other README documents',
+  Report: 'src/Report.*',
   release: 'Reserved for release commits.',
-  Setup: 'Affects "src/Setup.cs".',
+  Setup: 'src/Setup.cs',
   Social: 'src/Social.*',
-  Version: 'Affects "src/Version.*".',
-  TODO: 'Affects TODO.md or any todo comments.',
-  vscode: 'Affects "./vscode/**/*".',
+  Version: 'src/Version.*',
+  TODO: 'TODO.md or any todo comments.',
+  vscode: './vscode/**/*',
 };
 
 hceShared.rules['scope-enum'] = [
