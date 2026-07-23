b3b4d80c8ed973517ab3dda279349c7e95118168
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-23 04:03:28 -0700

    style(Splash): format

--- src/Splash.xaml.cs
+++ src/Splash.xaml.cs
@@ -14,21 +14,21 @@ using System.Windows.Shapes;
 
 namespace SPV3
 {
-    /// <summary>
-    /// Interaction logic for Splash.xaml
-    /// </summary>
-    public partial class Splash : Window
+  /// <summary>
+  /// Interaction logic for Splash.xaml
+  /// </summary>
+  public partial class Splash : Window
+  {
+    public Splash()
     {
-        public Splash()
-        {
-            WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            InitializeComponent();
-        }
-
-        public void QueueSplashClose()
-        {
-            System.Threading.Thread.Sleep(2000);
-            Close();
-        }
+      WindowStartupLocation = WindowStartupLocation.CenterScreen;
+      InitializeComponent();
     }
+
+    public void QueueSplashClose()
+    {
+      System.Threading.Thread.Sleep(2000);
+      Close();
+    }
+  }
 }
