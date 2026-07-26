88888e9b7b22b485c51502b3b281d98d0cc00d70
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-25 19:30:34 -0700

    refactor(Main): add nullable annotations

--- src/Main.Compile.cs
+++ src/Main.Compile.cs
@@ -42,10 +42,10 @@ namespace SPV3
         }
       }
 
-      public event PropertyChangedEventHandler PropertyChanged;
+      public event PropertyChangedEventHandler? PropertyChanged;
 
       [NotifyPropertyChangedInvocator]
-      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
       {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
       }
